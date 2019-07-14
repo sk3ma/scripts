4f97abadeff5334cd2aa06d6faa5430005abb019
sk3ma <52137438+sk3ma@users.noreply.github.com>
2019-07-14 17:54:47 +0200

    Add files via upload

---
+++ dokuwiki.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Checking Account
+if [[ $(/usr/bin/id -u) != "0" ]]; then
+    echo -e "You must be root to run this script."
+    exit
+fi
+# Apache Installation
+apache() {
+    echo "Installing Apache"
+    sudo apt update && sudo apt install apache2 apache2-utils libapache2-mod-php7.2 -y
+}
+# PHP Installation
+php() {
+    echo "Installing PHP"
+    sudo apt install php php-{curl,cli,dev,gd,mbstring,pear,zip} -y
+    echo "Configuringing PHP"
+    cat << EOF > /etc/php/7.2/apache2/php.ini
+file_uploads = On
+allow_url_fopen = On
+memory_limit = 256M
+upload_max_filesize = 100M
+max_execution_time = 360
+date.timezone = America/Chicago
+EOF
+    sudo systemctl restart apache2
+    echo "<?php phpinfo( ); ?>" | sudo tee /var/www/html/info.php
+}
+# DokuWiki Installation
+doku() {
+    echo "Installing DokuWiki"
+    sudo apt install git -y
+    cd /var/www/html/
+    sudo git clone --branch stable https://github.com/splitbrain/dokuwiki.git
+    echo "Changing Permissions"
+    sudo chown -R www-data:www-data /var/www/html/dokuwiki/
+    sudo chmod -R 755 /var/www/html/dokuwiki/
+}
+config() {
+    echo "Configuring DokuWiki"
+    cat << EOF > /etc/apache2/sites-available/dokuwiki.conf
+<VirtualHost *:80>
+    ServerAdmin admin@admlab.dev
+    DocumentRoot /var/www/html/dokuwiki
+    ServerName admlab.dev
+    ServerAlias www.admlab.dev
+
+     <Directory /var/www/html/dokuwiki/>
+          Options FollowSymlinks
+          AllowOverride All
+          Require all granted
+     </Directory>
+
+     ErrorLog ${APACHE_LOG_DIR}/error.log
+     CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+     <Directory /var/www/html/dokuwiki/>
+            RewriteEngine on
+            RewriteBase /
+            RewriteCond %{REQUEST_FILENAME} !-f
+            RewriteRule ^(.*) index.php [PT,L]
+    </Directory>
+</VirtualHost>
+EOF
+}
+# Adjusting Firewall
+fw() {
+    sudo ufw allow 8080/tcp
+}
+restart() {
+    echo "Restarting Service"
+    sudo a2ensite dokuwiki.conf
+    sudo a2enmod rewrite
+    sudo systemctl restart apache2
+    echo "Web Installation: http://admlab.dev/install.php"
+    exit
+}
+# Calling Functions
+apache
+php
+doku
+config
+fw
+restart
